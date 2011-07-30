4cb6ea9153c761c78e2f28a6600f67a4310bf5c7
Kevin Martensson <kevinmartensson@gmail.com>
2011-07-30 23:39:15 +0200

    small fix for consistent white space

--- inc/essence-cleanup.php
+++ inc/essence-cleanup.php
@@ -72,7 +72,7 @@ function essence_clean_plugins( $content ) {
 /**
  * Only use clean URLs if the theme isn't a child or an MU (Network) install
  */
-if ( (!defined( 'WP_ALLOW_MULTISITE' ) || ( defined( 'WP_ALLOW_MULTISITE' ) && WP_ALLOW_MULTISITE !== true) ) && !is_child_theme() ) {
+if ( ( !defined( 'WP_ALLOW_MULTISITE' ) || ( defined( 'WP_ALLOW_MULTISITE' ) && WP_ALLOW_MULTISITE !== true) ) && !is_child_theme() ) {
   add_action( 'generate_rewrite_rules', 'essence_add_rewrites' );
   add_filter( 'plugins_url', 'essence_clean_plugins' );
   add_filter( 'bloginfo', 'essence_clean_assets' );
