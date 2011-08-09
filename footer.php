3f93682c85a906042525d174679fcd883870284d
Kevin Martensson <kevinmartensson@gmail.com>
2011-08-09 09:08:10 +0200

    added google analytics. load through modernizr.load.

--- footer.php
+++ footer.php
@@ -21,6 +21,14 @@
   <script defer src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
   <!-- end scripts-->
 
+  <!-- Change UA-XXXXX-X to be your site's ID -->
+  <script>
+    window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
+    Modernizr.load({
+      load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+    });
+  </script>
+
   <!--[if lt IE 7 ]>
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
     <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
