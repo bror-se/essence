5a2fc754996a942f7cb1f7354d83cfe0b21e5f22
Kevin Martensson <kevinmartensson@gmail.com>
2011-08-15 21:12:06 +0200

    fixed regexp ignoring js

--- footer.php
+++ footer.php
@@ -17,10 +17,10 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
 
-  <!-- scripts concatenated and minified via ant build script-->
+  <!-- scripts concatenated and minified via build script -->
   <script defer src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
   <script defer src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
-  <!-- end scripts-->
+  <!-- end scripts -->
 
   <!-- Change UA-XXXXX-X to be your site's ID -->
   <script>
