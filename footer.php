a74c16e92347e9832b7eff497624b0623b228ec3
Kevin Martensson <kevinmartensson@gmail.com>
2011-12-29 16:24:37 +0100

    Fixed h2 closed by h1.

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 </footer>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/lib/jquery-1.7.1.min.js"><\/script>')</script>
 
 <!-- scripts concatenated and minified via build script -->
 <script defer src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
