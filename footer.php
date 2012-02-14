bfe1128c8a15712a656f93323f94a97804661506
Kevin Martensson <kevinmartensson@gmail.com>
2012-02-14 09:04:59 +0100

    Removed defer on script references due to IE bugs.

--- footer.php
+++ footer.php
@@ -7,10 +7,8 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
-  <!-- scripts concatenated and minified via build script -->
-  <script defer src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
-  <script defer src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
-  <!-- end scripts -->
+  <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
 
   <?php wp_footer(); ?>
 
